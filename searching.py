0d7ab2eac3d9c63f0078f4bde0b7d27135372492
Anything18years <mg.maxwellgyampohjr4@gmail.com>
2025-02-09 06:58:54 -0800

    Delete searching.py

--- searching.py
+++
@@ -1,7 +0,0 @@
-def linear_search(haystack, needle):
-    for element in haystack:
-        if element == needle:
-            return True 
-    return False
-some_list = range(1,10000000)
-print(linear_search(some_list,0))
